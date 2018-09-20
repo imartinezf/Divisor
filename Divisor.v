1d19f2ec0030a8b4e6119a31e743850b348fd8d7
imartinezf <es41551@iteso.mx>
2018-09-20 18:49:27 -0500

    Update final version

--- Divisor.v
+++ Divisor.v
@@ -1,52 +1,45 @@
- /******************************************************************* 
+/*********************** 
 * Name:
 *	Decodificador.v
 * Description:
 * Clock divider circuit
 * From 50 MHz to 1 MHz with %50 duty cycle
 * Inputs:
-*	Clk_in: Clock signal 
-* 	reset: reset signal	 
+*	clk: Input Clock 50000000  
 * Outputs:
-* 	Clk_out: Clk out with 50% duty cycle
+*  flag: Clk out with 50% duty cycle
 * Version:  
 *	1.0
 * Author: 
 *	Ivan Martinez Flores & Jorge Araiza Martinez 
 * Fecha: 
 *	18/09/2018
-*********************************************************************/
-
+***********************/
 module Divisor
-#(// reference_clocck frequency 50MHz, frequency 1 MHz
-	parameter frecuency = 1000000,
-	parameter reference_clocck = 50000000,
-	parameter max = frecuency / (2 * reference_clocck)
-)  
 (
-
-// input ports
-	input Clk_in,
-	input reset,
-	
-// output ports
-	output reg Clk_out = 1'b0
+	// Input Ports
+	input clk,
+	// Output Ports
+	output reg flag
 );
+	// Parameter Declarations
+	parameter Frequency = 1_000_000;
+	parameter Reference_Colcck = 50_000_000;
+	parameter max = Reference_Colcck/(2*Frequency);
+	
+	reg [22:0] count;
+	initial begin
+	count = 0;
+	flag= 0;
+	end
 
-reg [4:0]counter = 0; // 5-bit counter size
-
-always@(posedge Clk_in or negedge reset) begin
-	if (reset == 1'b0)
-		counter <= 0;	
-	if (counter == max-1)
-		begin
-		counter <= 0;
-		Clk_out <= ~Clk_out;
-		end
-	else
-		begin
-		counter <= counter + 1'd1;
-		end
-
+	always@(posedge clk) begin
+		if(count == (max)) begin
+			flag = ~flag;
+			count = 0;
+	end
+	else begin
+		count = count +1;
+	end
 	end
 endmodule
